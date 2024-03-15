43b60deadc4101617c3a7b0092d9cdcc6cee6db2
linexy <linexy@naver.com>
2024-03-15 22:05:08 +0900

    Initial Commit

---
+++ SwitUIIOS16Basic/SwitUIIOS16BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitUIIOS16BasicApp.swift
+//  SwitUIIOS16Basic
+//
+//  Created by 김어진 on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwitUIIOS16BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
